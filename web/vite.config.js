f375402b25c277cc7278ee2571bfc4beae87acb6
Sarit Sotangkur <ssotangkur@yahoo.com>
2026-01-03 00:58:13 -0800

    setup vite for bundled build

--- web/vite.config.js
+++ web/vite.config.js
@@ -4,16 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   root: '.',
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM'
-        }
-      }
-    }
-  }
+  plugins: [
+    react(),
+  ],
 })
